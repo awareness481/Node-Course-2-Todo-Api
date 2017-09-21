c7ca092b8e00a44b1f8a20fd83fbf317bd171d13
Jesse Jafa <jesse.jafa@gmail.com>
2017-09-21 12:05:23 +0300

    Added DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -53,6 +53,22 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(doc => {
+    if (!doc)
+      return res.status(404).send();
+
+    return res.status(200).send();
+  }).catch((e)  => {
+    return res.status(400).send(doc);
+  });
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
